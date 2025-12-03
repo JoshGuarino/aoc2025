c79cddf1fb7066f89aedd2ac8a9133e1350c145d
Josh Guarino <jguarino722@gmail.com>
2025-12-03 12:17:37 -0500

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -1,6 +1,3 @@
-from os import wait
-
-
 def load_input():
     id_ranges = []
     with open("input.txt") as file:
@@ -9,17 +6,14 @@ def load_input():
         for line in lines:
             range = line.split("-")
             id_ranges.append((int(range[0]), int(range[1])))
-
     return id_ranges 
 
-def check_id_valid(id: int) -> bool:
+def check_id_valid1(id: int) -> bool:
     string_id = str(id)
     length = len(string_id)
 
-    # Check if the length is odd, 
-    # odd numbers can't be palindromes
-    # meaning they don't fit the criteria
-    # for and invalid id
+    # Check if the length is odd, odd numbers can't be palindromes
+    # meaning they don't fit the criteria for an invalid id
     if length % 2 != 0:
         return True 
     
@@ -29,27 +23,40 @@ def check_id_valid(id: int) -> bool:
     second_half = string_id[mid:]
 
     # Check if the first half is a palindrome
-    # invlid ids should be the same
+    # invalid ids should be the same
     # based on the criteria for an invalid id
     return first_half != second_half
 
-def part1(id_ranges: list):
-    invalid_ids = []
+def check_id_valid2(id: int) -> bool:
+    string_id = str(id)
+    length = len(string_id)
 
-    # Check every id in the range
-    for id_range in id_ranges:
-        for i in range(id_range[0], id_range[1]):
-            if not check_id_valid(i):
-                invalid_ids.append(i)
-
-    # Sum the invalid ids
-    return sum(invalid_ids)
+    # An ID is invalid if it is made of a repeating sequence of digits
+    # The length of the sequence can be from 1 to half the length of the ID
+    for i in range(1, length // 2 + 1):
+        # The length of the ID must be a multiple of the sequence length
+        if length % i == 0:
+            sequence = string_id[:i]
+            repetitions = length // i
+            if sequence * repetitions == string_id:
+                # Found a repeating sequence, so the ID is invalid
+                return False
+    
+    # No repeating sequence found, so the ID is valid
+    return True 
 
 def main():
     id_ranges = load_input()
-    sum1 = part1(id_ranges)
+    invalid_ids1 = []
+    invalid_ids2 = []
+    for id_range in id_ranges:
+        for i in range(id_range[0], id_range[1]):
+            if not check_id_valid1(i):
+                invalid_ids1.append(i)
+            if not check_id_valid2(i):
+                invalid_ids2.append(i)
 
-    return sum1 
+    return sum(invalid_ids1), sum(invalid_ids2)
 
 if __name__ == "__main__":
     answer = main()

e17a92e0d80428fc4d80fa8abc20bb8c107ae5bf
Josh Guarino <jguarino722@gmail.com>
2025-12-10 11:13:41 -0500

    day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -5,9 +5,8 @@ def load_input():
         banks = [[int(i) for i in line] for line in lines]
     return banks
 
-def main():
+def part1(banks: list[list[int]]) -> int:
     joltages = []
-    banks = load_input()
     for bank in  banks:
         max_val = 0
         for i in range(len(bank)):
@@ -19,6 +18,47 @@ def main():
         joltages.append(max_val)
     
     return sum(joltages) 
+
+def part2(banks: list[list[int]]) -> int:
+    TARGET_LENGTH = 12
+    joltages = []
+
+    for bank in banks:
+        # Number of digits to remove (drop)
+        k = len(bank) - TARGET_LENGTH
+        
+        # 'result' will store the digits we select, forming the largest number
+        result = []
+        
+        for digit in bank:
+            # Greedy step: While we have drops left (k > 0), and the result is 
+            # not empty, AND the current digit is greater than the last digit 
+            # in our result, we drop the last digit from the result.
+            while k > 0 and result and digit > result[-1]:
+                result.pop()
+                k -= 1
+            
+            # Add the current digit to the result list
+            result.append(digit)
+            
+        # If we still have drops left after iterating through all digits, 
+        # it means the smallest digits were at the end. Trim the result list.
+        # Note: Since the bank length is 15 and TARGET_LENGTH is 12, 
+        # this trimming is equivalent to just taking the first TARGET_LENGTH elements.
+        final_digits = result[:TARGET_LENGTH]
+        
+        joltage_str = "".join(map(str, final_digits))
+        joltages.append(int(joltage_str))
+
+    return sum(joltages)
+  
+
+def main():
+    banks = load_input()
+    part1_answer = part1(banks)
+    part2_answer = part2(banks)
+
+    return part1_answer, part2_answer
 
 if __name__ == "__main__":
     answer = main()

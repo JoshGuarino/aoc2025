3b10d72ce8d71fd4663bd0c51e18a646d08c532b
Josh Guarino <jguarino722@gmail.com>
2025-12-16 12:57:18 -0500

    day 4 part 1

--- day4/main.py
+++ day4/main.py
@@ -1,2 +1,23 @@
+def load_input():
+    grid = []
+    with open("input.txt") as file:
+        input = file.read()
+        for line in input.splitlines():
+            row = []
+            for char in line:
+                row.append(char)
+            grid.append(row)
+    return grid
+
+def part1(grid: list[list[str]]) -> int:
+    paper_roll_count = 0
+    for row in grid:
+        pass # complete me
+    return paper_roll_count
+
+def main():
+    grid = load_input()
+    print(grid[0])
+
 if __name__ == "__main__":
-    pass
+    main()
